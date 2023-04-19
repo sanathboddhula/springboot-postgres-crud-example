1d7a6ed30a51095a4a7947448238c6258a9eff29
Sanath Boddhula <sanath.boddhula@gmail.com>
2023-04-18 23:08:07 -0400

    Database connection done

--- src/main/java/com/ahl/springbootpostgrescrudexample/datamodel/User.java
+++ src/main/java/com/ahl/springbootpostgrescrudexample/datamodel/User.java
@@ -1,6 +1,14 @@
 package com.ahl.springbootpostgrescrudexample.datamodel;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ads_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
@@ -8,61 +16,4 @@ public class User {
     private String location;
     private String password;
 
-    public User(int id, String name, String email, String phone, String location, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.location = location;
-        this.password = password;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
---
+++ src/test/java/com/ahl/springbootpostgrescrudexample/datamodel/UserTest.java
@@ -0,0 +1,14 @@
+package com.ahl.springbootpostgrescrudexample.datamodel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    //write a test to check if the user object is created successfully
+    @Test
+    void testThatUserObjectIsCreatedSuccessfully() {
+        User user = new User();
+
+    }
+}
