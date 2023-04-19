9911040d897799e4a46039a57390b5c56f8fe873
Sanath Boddhula <sanath.boddhula@gmail.com>
2023-04-18 22:11:57 -0400

    Initial commit

---
+++ src/main/java/com/ahl/springbootpostgrescrudexample/SpringbootPostgresCrudExampleApplication.java
@@ -0,0 +1,14 @@
+package com.ahl.springbootpostgrescrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresCrudExampleApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!!");
+        SpringApplication.run(SpringbootPostgresCrudExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahl/springbootpostgrescrudexample/SpringbootPostgresCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahl.springbootpostgrescrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresCrudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
